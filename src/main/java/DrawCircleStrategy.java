4c922ae2b4b9e0bf1e98eda81590667297f9f1e4
prego123 <shalu.pragati11@gmail.com>
2022-04-04 20:14:19 +0530

    figures

--- src/main/java/DrawCircleStrategy.java
+++ src/main/java/DrawCircleStrategy.java
@@ -1,7 +1,7 @@
 public class DrawCircleStrategy implements DrawFigureStrategy{
     int x[] = new int[4];
     public void drawFigure(int WIDTH, int HEIGHT, int DIAMETER){
-        System.out.println("hello");
+        System.out.println("Ellipse");
         for(int i=1; i<5; i++)
         {
             //ellipse(x[i-1]+=i, HEIGHT*i/5, DIAMETER, DIAMETER);
---
+++ src/main/java/DrawRectangleStrategy.java
@@ -0,0 +1,5 @@
+public class DrawRectangleStrategy implements DrawFigureStrategy{
+    public void drawFigure(int WIDTH, int HEIGHT, int DIAMETER){
+        System.out.println("Rectangle");
+    }
+}
